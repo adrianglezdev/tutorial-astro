4c45014981c7c043877c86e17a29c90641819ad5
Adrián González <prueba_de_git@piton.com>
2025-08-15 00:28:00 +0100

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
 export default defineConfig({
-  site: "https://adrianglezdev.github.io",
-  base: "/tutorial-astro",
+  site: "https://example.com",
 
   integrations: [preact()]
 });
